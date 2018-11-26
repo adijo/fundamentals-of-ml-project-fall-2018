d4cee9c2d45955989f072a911e470481ee230597
hlafortu <44884745+hlafortu@users.noreply.github.com>
2018-11-26 11:38:40 -0500

    Add files via upload
    I change the banking preprocessing. I change from Labelencoding to one onhotencoding. It makes more sense to use this since the categorical variable dont have an order in them. Also "duration" was not really dropped (drop on pandas is not inplace by default). Now it is correctly drop. Using one-hot-encoding should improve our prediction however dropping duration will make the prediction worse.
    
    I recreate the test,train using this for the Banking data.

--- banking_transform.py
+++ banking_transform.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
+
+# In[2]:
 
 
 df = pd.read_csv('data/banking/bank-additional-full.csv', sep=';')
@@ -14,18 +24,89 @@ columns = ["job",
            "contact",
            "month",
            "day_of_week",
-           "poutcome",
-           "y"]
+           "poutcome"]
 
+
+# In[3]:
+
+
+Numeric_columns=["age",
+                 "duration",
+                 "campaign",
+                 "pdays",
+                 "previous",
+                 "emp.var.rate",
+                 "cons.price.idx",
+                 "cons.conf.idx",
+                 "euribor3m",
+                 "nr.employed"]
+
+
+# In[4]:
+
+
+#Label encode
+classes={}
 for column in columns:
-    df[column] = LabelEncoder().fit_transform(df[column])
+    labelenc=LabelEncoder()
+    df[column] = labelenc.fit_transform(df[column])
+    classes[column]=labelenc.classes_
+
+
+# In[5]:
+
+
+labelenc=LabelEncoder()
+df["y"] = labelenc.fit_transform(df["y"])
+Numeric_columns+=["y"]
+
+
+# In[6]:
+
+
+#Create the new columns name
+columns_name_cat=[]
+for j in range(len(columns)):
+    for i in classes[columns[j]]:
+        columns_name_cat.append(columns[j]+"."+i)
+
+
+# In[7]:
+
+
+databank_Categorical_onehot=OneHotEncoder().fit_transform(np.array(df[columns])).toarray()
+
+
+# In[8]:
+
+
+df=np.append(databank_Categorical_onehot,np.array(df[Numeric_columns]),axis=1)
+
+
+# In[9]:
+
+
+df=pd.DataFrame(df,columns=np.append(columns_name_cat,Numeric_columns))
+
+
+# In[10]:
 
 
 # This column should be dropped for a realistic predictive model:
 # Please read Attribute Information
 # on this page:https://archive.ics.uci.edu/ml/datasets/bank+marketing
-df.drop("duration", axis=1)
+df=df.drop("duration", axis=1)
+
+
+# In[11]:
+
 
 train, test = train_test_split(df, test_size=0.2)
+
+
+# In[12]:
+
+
 train.to_csv("data/banking/bank-additional-full-transformed-train.csv", sep=";", index=False)
 test.to_csv("data/banking/bank-additional-full-transformed-test.csv", sep=";", index=False)
+
